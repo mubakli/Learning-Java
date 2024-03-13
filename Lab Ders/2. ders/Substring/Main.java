7f52034f142a1a02f2f4ef66b85394091a4a91e6
Burak Asarcıklı <brkasarcikli@outlook.com>
2024-03-13 13:58:57 +0300

    LabLess

---
+++ Lab Ders/2. ders/Substring/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+        public static void main(String[] args) {
+            String str = "ababcabcde"; // Örnek bir giriş String'i
+
+            public String findRepeatingSubs(String ){
+            for( int i = 0; i < str.length() ; i++){
+                for ( int j = i+1; j <= str.length() ; i++){
+                    String substring = str.substring( i , j);
+
+                }   }
+            }
+
+
+
+
+        }
+
+
+
+
+
+}
